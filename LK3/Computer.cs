8262db0eb41e55b3391ef79692876bdd773762cd
ShabaNure <maksym.shabelnikov@nure.ua>
2023-04-06 16:37:07 +0200

    LK3

--- LK3/Computer.cs
+++ LK3/Computer.cs
@@ -4,34 +4,36 @@ namespace LK3
 {
     public class Computer:IDisposable
     {
+        private List<IRam> rams= new List<IRam>();
+        private List<IHardDrive> hardDrives = new List<IHardDrive>(); 
+        private List<ICpu> cpus= new List<ICpu>();
         public void AddDevice(IDev device)
         {
             if(device is IRam)
             {
-                device.Eject();
+                rams.Add((IRam)device);
             }
             else if(device is IHardDrive)
             {
-                device.Remove();
+                hardDrives.Add((IHardDrive)device);
             }
             else if (device is ICpu)
             {
-                device.Unplug();
-            }
-            else
-            {
-                device.Disconnect();
+                cpus.Add((ICpu)device);
             }
 
         }
 
         public void Disconnect()
         {
-            Console.WriteLine("-----Nothing-----");
+            foreach (IRam device in rams) { device.Eject(); }
+            foreach (IHardDrive device in hardDrives) { device.Remove(); }
+            foreach (ICpu device in cpus) { device.Unplug();}
+            Console.WriteLine("-------All devices disconnected------");
         }
         public void Dispose()
         {
-            Console.WriteLine("-----All devices disconnected-----");
+            Disconnect();
         }
     }
 }
