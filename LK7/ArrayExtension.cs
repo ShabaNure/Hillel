86ed3660317fb181af124f6fec2624b022a04d84
ShabaNure <maksym.shabelnikov@nure.ua>
2023-04-15 11:34:11 +0200

    LK7

---
+++ LK7/ArrayExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LK7
+{
+    public static class ArrayExtension
+    {
+        public static void Calc(int x, int y, double[] mas, HashSet<double> set)
+        {
+            for (int i = x; i < y; i++)
+            {
+                double temp = Math.Sqrt(mas[i]);
+                if (Convert.ToInt32(temp) == Convert.ToDouble(temp))
+                    set.Add(temp);
+                
+            }
+        }
+    }
+    
+    
+}
---
+++ LK7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LK7
+{
+    public class Program
+    {
+
+        public delegate void Del(int x, int y, double[] mas, HashSet<double> set);
+        static void Main()
+        {
+            var del = new Del(ArrayExtension.Calc);
+            double[] mas = new double[100000];
+            HashSet<double> lst = new HashSet<double>();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = i;
+            }
+
+            var thread1 = new Thread(() =>{ del(0, 50000, mas, lst); Console.WriteLine("Thread 1 - Finished"); });
+            var thread2 = new Thread(() =>{ del(50000, 100000, mas, lst); Console.WriteLine("Thread 2 - Finished"); });
+            thread1.Start();
+            thread1.Join();
+            thread2.Start();
+            thread2.Join();
+            Console.WriteLine("-----Proccess finished-----");
+            foreach(var x in lst)
+            Console.WriteLine(x);
+        }
+        
+        
+    }
+}
