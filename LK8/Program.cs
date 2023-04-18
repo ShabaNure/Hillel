843a5e4beeaa9cac5ddb9dfc874116402926ffb8
ShabaNure <maksym.shabelnikov@nure.ua>
2023-04-18 13:48:37 +0200

    LK8

---
+++ LK8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LK8
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Thread[] threads = new Thread[10];
+            var rnd = new Random();
+            int positive = 0;
+            int negative = 0;
+            var lockObj = new object();
+            for(int i =0;i<threads.Length;i++)
+            {
+                threads[i] = new Thread(() => 
+                {
+                    for(int j =0;j<100;j++)
+                    {
+                        lock(lockObj)
+                        {
+                            var num = rnd.Next(1, 100);
+                            if (num % 2 == 0)
+                            {
+                                positive++;
+                            }
+                            else negative++;
+                        }
+                    }
+                });
+            }
+            foreach (var thrd in threads)
+            {
+                thrd.Start();
+            }
+            foreach (var thrd in threads)
+            {
+                thrd.Join();
+            }
+            Console.WriteLine($"Pos - {positive} Neg - {negative}");
+        }
+    }
+}
