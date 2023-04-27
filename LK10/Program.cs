1db17cb2b611bb13ab0200357e76b6939cb3dca1
ShabaNure <maksym.shabelnikov@nure.ua>
2023-04-27 10:34:21 +0200

    LK10

---
+++ LK10/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LK10
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            Tasks();
+            Thread.Sleep(1000);
+        }
+
+        static async void Tasks()
+        {
+            Task[] tasks = new Task[10];
+            var rnd = new Random();
+            int positive = 0;
+            int negative = 0;
+            var lockObj1 = new object();
+            var lockObj2 = new object();
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        var num = rnd.Next(1, 100);
+                        if (num % 2 == 0)
+                        {
+                            lock (lockObj1)
+                            {
+                                positive++;
+                            }
+                        }
+                        else
+                        {
+                            lock (lockObj2)
+                            {
+                                negative++;
+                            }
+                        }
+
+                    }
+                });
+                await tasks[i];
+            }
+
+            Console.WriteLine($"Pos - {positive} Neg - {negative}");
+        }
+    }
+}
