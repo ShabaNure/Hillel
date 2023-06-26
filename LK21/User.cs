cde16cb1af46712bf4ba792a3783d9974e257e2e
ShabaNure <maksym.shabelnikov@nure.ua>
2023-06-26 17:26:04 +0200

    LK21

---
+++ LK21/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LK20
+{
+    public class User : IUser
+    {
+        [Required(ErrorMessage ="Id is empty")]
+        [Range(1,10)]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Name is empty")]
+        [StringLength(15)]
+        [MinLength(2)]
+        public string Name { get; set; }
+        [Range(12,100)]
+        [Required(ErrorMessage = "age is empty")]
+        public int Age { get; set; }
+    }
+}
