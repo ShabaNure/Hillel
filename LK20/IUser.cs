e868821f52ec9bcadfc30034a8190121d680e1b0
ShabaNure <maksym.shabelnikov@nure.ua>
2023-06-26 16:25:45 +0200

    LK20

---
+++ LK20/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace LK20
+{
+    public interface IUser
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+        int UserId { get; set; }
+    }
+}
---
+++ LK20/User.cs
@@ -0,0 +1,9 @@
+﻿namespace LK20
+{
+    public class User : IUser
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
